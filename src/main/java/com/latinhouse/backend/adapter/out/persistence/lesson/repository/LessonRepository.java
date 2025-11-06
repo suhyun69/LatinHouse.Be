4470a6407089994393313a454dcae31ba93badea
suhyun.p69 <suhyun.p69@gmail.com>
2025-11-06 14:32:52 +0900

    getLessons

--- src/main/java/com/latinhouse/backend/adapter/out/persistence/lesson/repository/LessonRepository.java
+++ src/main/java/com/latinhouse/backend/adapter/out/persistence/lesson/repository/LessonRepository.java
@@ -2,9 +2,15 @@ package com.latinhouse.backend.adapter.out.persistence.lesson.repository;
 
 import com.latinhouse.backend.adapter.out.persistence.lesson.entity.LessonJpaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LessonRepository extends JpaRepository<LessonJpaEntity, Long> {
     Optional<LessonJpaEntity> findByNo(Long no);
+
+    @Query("SELECT l FROM LessonJpaEntity l WHERE l.instructorLo = :profileId OR l.instructorLa = :profileId")
+    List<LessonJpaEntity> findByProfileId(@Param("profileId") String profileId);
 }
